cc6b0ae7f444a19782b4cba892ef0247615a7eed
ellisthomas <ellisthomas80@gmail.com>
2017-10-16 19:59:11 -0500

    Project setup

---
+++ Workforce-Management/Migrations/Configuration.cs
@@ -0,0 +1,31 @@
+namespace Workforce_Management.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Workforce_Management.Models.WorkforceManagement>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(Workforce_Management.Models.WorkforceManagement context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
---
+++ Workforce-Management/Models/WorkforceManagmentContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace Workforce_Management.Models
+{
+    public class WorkforceManagement   :DbContext
+    {
+        public WorkforceManagement() : base("name=WorkforceManagement")
+        {
+        }
+        public System.Data.Entity.DbSet<Workforce_Management.Models.Computer>Computer { get; set; }
+
+        public System.Data.Entity.DbSet<Workforce_Management.Models.Departement> Department { get; set; }
+
+        public System.Data.Entity.DbSet<Workforce_Management.Models.Employee> Employee { get; set; }
+
+        public System.Data.Entity.DbSet<Workforce_Management.Models.TrainingProgram> TrainingProgram { get; set; }
+    }
+  
+}
