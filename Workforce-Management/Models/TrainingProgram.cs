14eb1e60bcca112a8539d2a4d9dcbdceed45b81b
Isabel Ramos <iramos1226@gmail.com>
2017-10-23 21:34:37 -0500

    datetime null prob

--- Workforce-Management/Models/TrainingProgram.cs
+++ Workforce-Management/Models/TrainingProgram.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace Workforce_Management.Models
 {
@@ -10,8 +11,12 @@ namespace Workforce_Management.Models
         public int TrainingProgramId { get; set; }
         public string TrainingProgramName { get; set; }
         public string Description { get; set; }
-        public DateTime StartDate { get; set; }
-        public DateTime EndDate { get; set; }
+        [DataType(DataType.Date)]
+        [Display(Name = "Start Date")]
+        public virtual DateTime? StartDate { get; set; }
+        [DataType(DataType.Date)]
+        [Display(Name = "End Date")]
+        public virtual DateTime? EndDate { get; set; }
         public int MaxAttendees { get; set; }
         public virtual List<Employee> Employee { get; set; }
     }
