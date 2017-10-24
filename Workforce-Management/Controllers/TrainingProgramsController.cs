1b54b79d2c77d3d97479abbbd4993528317d979a
Isabel Ramos <iramos1226@gmail.com>
2017-10-24 18:55:36 -0500

    create training program works

--- Workforce-Management/Controllers/TrainingProgramsController.cs
+++ Workforce-Management/Controllers/TrainingProgramsController.cs
@@ -46,7 +46,7 @@ namespace Workforce_Management.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "TrainingProgramId,TrainingProgramName")] TrainingProgram trainingProgram)
+        public ActionResult Create([Bind(Include = "TrainingProgramId,TrainingProgramName,Description,StartDate,EndDate,MaxAttendees")] TrainingProgram trainingProgram)
         {
             if (ModelState.IsValid)
             {
