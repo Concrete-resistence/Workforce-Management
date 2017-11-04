46f7c77a32860e95c2b3e0797197cb84fd32097a
Sanyyouisf <sanymamdoh@yahoo.com>
2017-11-04 10:32:17 -0500

    fix some bugs

--- Workforce-Management/Controllers/EmployeesController.cs
+++ Workforce-Management/Controllers/EmployeesController.cs
@@ -49,8 +49,8 @@ namespace Workforce_Management.Controllers
 
         private void DepartmentsDropDownList(object selectedDepartment = null)
         {
-          
-            ViewBag.DepartmentID = new SelectList(db.Departments.OrderBy(d => d.DepartmentId), "DepartmentId", "DepartmentName", selectedDepartment);
+
+            ViewBag.DepartmentID = new SelectList(db.Department.OrderBy(d => d.DepartmentId), "DepartmentId", "DepartmentName", selectedDepartment);
         }
 
 
@@ -61,12 +61,13 @@ namespace Workforce_Management.Controllers
         }
 
 
+
         // POST: Employees/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "EmployeeId,EmployeeFirstName,EmployeeLastName,HiringDate,ComputerId,DepartementId")] Employee employee)
+        public ActionResult Create([Bind(Include = "EmployeeId,EmployeeFirstName,EmployeeLastName,HiringDate,ComputerId,DepartmentId")] Employee employee)
         {
             if (ModelState.IsValid)
             {
@@ -74,6 +75,7 @@ namespace Workforce_Management.Controllers
                 try
                 {
                     db.Employee.Add(employee);
+                    db.Computer.Find(employee.ComputerId).Avaliable = false;
                     db.SaveChanges();
                     return RedirectToAction("Index");
                 }
@@ -100,7 +102,7 @@ namespace Workforce_Management.Controllers
                 return HttpNotFound();
             }
             ViewBag.AvailableCourses = db.TrainingProgram.ToList();
-            ViewBag.AvailableDepartments = db.Departments.ToList();
+            ViewBag.AvailableDepartments = db.Department.ToList();
             ViewBag.AvailableComputers = db.Computer
                 .Where(C => C.Avaliable == true)
                 .ToList();
@@ -134,7 +136,7 @@ namespace Workforce_Management.Controllers
             }
             //(new System.Collections.Generic.Mscorlib_CollectionDebugView<Workforce_Management.Models.Employee>(db.Employee.Local).Items[0]).ComputerId
             ViewBag.AvailableCourses = db.TrainingProgram.ToList();
-            ViewBag.AvailableDepartments = db.Departments.ToList();
+            ViewBag.AvailableDepartments = db.Department.ToList();
             ViewBag.AvailableComputers = db.Computer
                                         .Where(C => C.Avaliable == true).ToList();
             return View(employee);
@@ -151,7 +153,7 @@ namespace Workforce_Management.Controllers
             db.SaveChanges();
             Employee employee = db.Employee.Find(empId);
             ViewBag.AvailableComputers = db.Computer.ToList();
-            ViewBag.AvailableDepartments = db.Departments.ToList();
+            ViewBag.AvailableDepartments = db.Department.ToList();
             ViewBag.AvailableComputers = db.Computer
                                         .Where(C => C.Avaliable == true).ToList();
             return View("Edit", employee);
@@ -168,7 +170,7 @@ namespace Workforce_Management.Controllers
 
             db.SaveChanges();
             Employee employee = db.Employee.Find(empid);
-            ViewBag.AvailableDepartments = db.Departments.ToList();
+            ViewBag.AvailableDepartments = db.Department.ToList();
             ViewBag.AvailableComputers = db.Computer
                            .Where(C => C.Avaliable == true).ToList();
             return View("edit", employee);
